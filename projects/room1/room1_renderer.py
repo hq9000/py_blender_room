27318d0c756c150b356a9eba0b63e36c08398fb2
Sergey Grechin <grechin.sergey@gmail.com>
2020-08-21 08:44:17 +0300

    box mesh factory added etc.

--- projects/room1/room1_renderer.py
+++ projects/room1/room1_renderer.py
@@ -1,7 +1,68 @@
+import abc
+
 from framework.object import Object
 from framework.object_renderer import ObjectRenderer
+from projects.room1.room1_scene import Wall
+
+import bpy
+
+
+class RenderingStrategy:
+    @abc.abstractmethod
+    def render(self, obj: Object):
+        pass
+
+
+_id: int = 0
+
+
+def generate_new_id() -> str:
+    global _id
+    _id += 1
+    return str(_id)
+
+
+def create_box_mesh(size_x: float, size_y: float, size_z: float):
+    """
+
+    http://i.imgur.com/t0VGg09.png
+
+    :param size_x:
+    :param size_y:
+    :param size_z:
+    :return:
+    """
+    points = [
+        [0, 0, 0],
+        [size_x, 0, 0],
+        [size_x, size_y, 0],
+        [0, size_y, 0],
+
+        [0, 0, size_z],
+        [size_x, 0, size_z],
+        [size_x, size_y, size_z],
+        [0, size_y, size_z],
+    ]
+
+    faces = [
+        [1, 6, 5], [1, 6, 2],
+        [4, 6, 7], [7, 4, 3],
+        [3, 8, 4], [3, 7, 8],
+        [5, 8, 7], [5, 6, 7],
+        [1, 5, 8], [8, 1, 4],
+        [1, 4, 3], [1, 2, 3],
+    ]
+
+    return bpy.data.meshes.new(generate_new_id())
+
+
+class WallRenderingStrategy(RenderingStrategy):
+
+    def render(self, wall: Wall):
+        mesh = create_box_mesh(wall.thickness, wall.width, wall.height)
 
 
 class Room1ObjectRenderer(ObjectRenderer):
     def render_object(self, obj: Object):
-        print('preved ' + str(type(obj)))
+        if isinstance(obj, Wall):
+            WallRenderingStrategy().render(obj)
--- projects/room1/room1_scene.py
+++ projects/room1/room1_scene.py
@@ -1,16 +1,40 @@
 from dataclasses import dataclass
+from math import sqrt
+from typing import List
 
 from framework.entity import Entity
+from framework.point import Point
 from framework.scene import Scene
+
+
+@dataclass
+class Window:
+    width: float
+    height: float
+    margin_bottom: float
+    margin_left: float
 
 
 @dataclass
 class Wall(Entity):
     thickness: float
     height: float
-    width: float
+    x0: float
+    y0: float
+    x1: float
+    y1: float
+    windows: List[Window]
+
+    @property
+    def width(self) -> float:
+        delta_x = self.x1 - self.x0
+        delta_y = self.y1 - self.y0
+        return sqrt(pow(delta_x, 2) + pow(delta_y, 2))
 
 
 class Room1Scene(Scene):
     def _build(self):
-        self.objects.append(Wall(thickness=0.2, height=5, width=10))
+        window = Window(margin_bottom=1, height=2, width=1.5, margin_left=1)
+
+        wall_a = Wall(thickness=0.2, height=5, windows=[window], x0=0, y0=0, x1=10, y1=10)
+        self.objects.append(wall_a)
