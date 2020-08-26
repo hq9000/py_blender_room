58ed8f8defa35fc400a4061a6f2e6b24fb7c9aae
Sergey Grechin <grechin.sergey@gmail.com>
2020-08-26 06:27:36 +0300

    configurable roughness

--- py_blender_room/framework/material.py
+++ py_blender_room/framework/material.py
@@ -2,11 +2,12 @@ from dataclasses import dataclass
 from typing import Tuple
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class Material:
     name: str
     texture_file_path: str
     metallic: float = 0.55
+    roughness: float = 0.5
     scale: Tuple[float, float, float] = (1, 1, 1)
     rotation: Tuple[float, float, float] = (0, 0, 0)
-
+    displacement: bool = True
